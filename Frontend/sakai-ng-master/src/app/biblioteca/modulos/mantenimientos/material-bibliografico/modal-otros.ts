105d81096804135fa6685860bbdd642103524922
Pedro Morán <moranpedro0398@gmail.com>
2025-06-08 13:39:56 -0500

    Cambios

--- Frontend/sakai-ng-master/src/app/biblioteca/modulos/mantenimientos/material-bibliografico/modal-otros.ts
+++ Frontend/sakai-ng-master/src/app/biblioteca/modulos/mantenimientos/material-bibliografico/modal-otros.ts
@@ -635,11 +635,36 @@ export class ModalOtrosComponent implements OnInit {
         }
     }
     async ListaDetalle() {
+        const idBib = this.objetoOtro?.id;
+        if (!idBib) { return; }
         try {
-            const result: any = await this.materialBibliograficoService.lista_ejemplares('material-bibliografico/ciudad').toPromise();
-            if (result.status === "0") {
-                this.detalles = result.data;
-            }
+            const data = await this.materialBibliograficoService
+                .listarDetallesPorBiblioteca(idBib, false)
+                .toPromise();
+
+            this.detalles = (data ?? []).map(d => {
+                const sedeId  = d.codigoSede ?? d.biblioteca?.sedeId ?? null;
+                const tipoMat = d.tipoMaterialId ?? d.biblioteca?.tipoMaterialId ?? null;
+                const tipoAdq = d.tipoAdquisicionId ?? d.biblioteca?.tipoAdquisicionId ?? null;
+
+                const sedeObj      = this.sedesLista.find(s => s.id === sedeId) ?? null;
+                const tipoMatObj   = this.tipoMaterialLista.find(t => t.id === tipoMat) ?? null;
+                const tipoAdqObj   = this.tipoAdquisicionLista.find(t => t.id === tipoAdq) ?? null;
+
+                return {
+                    idDetalleBiblioteca: d.idDetalleBiblioteca,
+                    codigoSede: sedeId,
+                    tipoMaterialId: tipoMat,
+                    tipoAdquisicionId: tipoAdq,
+                    costo: d.costo ?? null,
+                    numeroFactura: d.numeroFactura ?? null,
+                    fechaIngreso: d.fechaIngreso ?? null,
+                    sede: sedeObj,
+                    tipoMaterial: tipoMatObj,
+                    tipoAdquisicion: tipoAdqObj,
+                    idEstado: d.idEstado
+                } as DetalleDisplay;
+            });
         } catch (error) {
             console.log(error);
             this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Ocurrió un error. No se pudo cargar especialidad' });
@@ -733,9 +758,15 @@ export class ModalOtrosComponent implements OnInit {
                 acceptLabel: 'SI',
                 rejectLabel: 'NO',
                 accept: () => {
-                  const sedeId  = this.formDetalle.value.sede as number;
-                  const tipoMat = this.formDetalle.value.tipoMaterial as number | null;
-                  const tipoAdq = this.formDetalle.value.tipoAdquisicion as number | null;
+                  const sedeVal  = this.formDetalle.value.sede;
+                  const tipoMatVal = this.formDetalle.value.tipoMaterial;
+                  const tipoAdqVal = this.formDetalle.value.tipoAdquisicion;
+
+                  const sedeId  = typeof sedeVal === 'object' ? sedeVal?.id : sedeVal;
+                  const tipoMat =
+                    typeof tipoMatVal === 'object' ? tipoMatVal?.id : (tipoMatVal ?? null);
+                  const tipoAdq =
+                    typeof tipoAdqVal === 'object' ? tipoAdqVal?.id : (tipoAdqVal ?? null);
 
                   const detalle: DetalleDisplay = {
                     codigoSede:        sedeId,
